321fa4a707f4b061c48dd5b669b225a8bfe23541
Roman Zhuravlov <snelsithepirate@gmail.com>
2021-03-24 13:44:05 +0200

    Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["alloy", "alloy/react", "alloy/typescript"],
+  extends: ["alloy", "alloy/react", "alloy/typescript", "react-hooks"],
   env: {
     browser: true,
   },
@@ -11,5 +11,12 @@ module.exports = {
   rules: {
     "@typescript-eslint/no-empty-interface": "off",
     "@typescript-eslint/explicit-member-accessibility": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": [
+      "warn",
+      {
+        additionalHooks: "useRecoilCallback",
+      },
+    ],
   },
 };
